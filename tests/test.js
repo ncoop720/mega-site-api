eebf1553d6bd6e9f3420feecd275d1289c90b1f1
Nathan Cooper <ncoop720@yahoo.com>
2020-09-27 16:31:53 -0400

    change package.json package info, remove tests

--- tests/test.js
+++ tests/test.js
@@ -7,11 +7,5 @@ const app = require('../app.js');
 const request = supertest(app);
 
 describe('Tests app', function() {
-  it('verifies get', function(done) {
-    request.get('/').expect(200).end(function(err, result) {
-        test.string(result.body.Output).contains('Hello');
-        test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
-        done(err);
-    });
-  });
+  // tests
 });
